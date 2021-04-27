45c4e6f6477eee315c538f44c0c6d89d589f0b3c
Jiayu Liu <jiayu.liu@airbnb.com>
2021-04-27 10:43:46 +0800

    update dockerfile

--- src/main/java/nginx/geoip/aliyun/AliyunEnrichGeoIPHeaders.java
+++ src/main/java/nginx/geoip/aliyun/AliyunEnrichGeoIPHeaders.java
@@ -1,7 +1,10 @@
 package nginx.geoip.aliyun;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.Map;
+
+import nginx.clojure.NginxClojureRT;
 import nginx.clojure.java.Constants;
 import nginx.clojure.java.NginxJavaHeaderFilter;
 
@@ -10,6 +13,8 @@ public class AliyunEnrichGeoIPHeaders implements NginxJavaHeaderFilter {
   public Object[] doFilter(
       int status, Map<String, Object> request, Map<String, Object> responseHeaders)
       throws IOException {
+    NginxClojureRT.log.info("AliyunEnrichGeoIPHeaders hit");
+    responseHeaders.put("jiayu-test-response", LocalDateTime.now().toString());
     return Constants.PHASE_DONE;
   }
 }
